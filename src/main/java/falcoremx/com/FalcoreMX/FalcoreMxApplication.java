92b5791ceccb9a685f6350ac6827c823d226d78e
carlosgr55 <124015981+carlosgr55@users.noreply.github.com>
2025-09-27 17:56:00 -0600

    carga

---
+++ src/main/java/falcoremx/com/FalcoreMX/FalcoreMxApplication.java
@@ -0,0 +1,13 @@
+package falcoremx.com.FalcoreMX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalcoreMxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalcoreMxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/falcoremx/com/FalcoreMX/FalcoreMxApplicationTests.java
@@ -0,0 +1,13 @@
+package falcoremx.com.FalcoreMX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalcoreMxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
