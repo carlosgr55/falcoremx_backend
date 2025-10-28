3831e8ddd748d1b7146d1c463ad693a4833bbb3e
carlosgr55 <124015981+carlosgr55@users.noreply.github.com>
2025-10-27 20:57:35 -0600

    products

--- src/main/java/falcoremx/com/FalcoreMX/controller/ProductController.java
+++ src/main/java/falcoremx/com/FalcoreMX/controller/ProductController.java
@@ -4,8 +4,12 @@ package falcoremx.com.FalcoreMX.controller;
 import falcoremx.com.FalcoreMX.entity.Product;
 import falcoremx.com.FalcoreMX.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +36,21 @@ public class ProductController {
     public List<Product> getProductsByEmpresaId(@PathVariable Integer idEmpresa) {
         return productService.findProductByEmpresa(idEmpresa);
 
+    }
+
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/products")
+    public Product updateProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable Integer id) {
+        productService.deleteProduct(id);
     }
 
 }
